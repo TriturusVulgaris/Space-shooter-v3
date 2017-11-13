0d0da1217e6c1002fb891cd667fec709dda3b6c4
Tristan Jardon <tristan.jardon@gmail.com>
2017-11-13 15:23:51 +0100

    Add functionnal asteroid spawner

---
+++ GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    [SerializeField]
+    Transform[] spawner;
+
+    [SerializeField]
+    GameObject asteroid;
+
+    [SerializeField]
+    float cooldown = 0.5f;
+    float buffer = 0f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(buffer >= cooldown) {
+            int randomspawn = Random.Range(0, 5);
+            Instantiate(asteroid, spawner[randomspawn].position, spawner[randomspawn].rotation);
+            buffer = 0f;
+        }
+        else {
+            buffer += Time.deltaTime;
+        }
+		
+	}
+}
