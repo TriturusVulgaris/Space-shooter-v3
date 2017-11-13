f5177f6df6a36221b973780fa4008b916ebc7ec4
Tristan Jardon <tristan.jardon@gmail.com>
2017-11-13 10:58:06 +0100

    First commit

---
+++ Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField]
+    Rigidbody body;
+    [SerializeField]
+    float speed = 10f;
+
+    // Use this for initialization
+    void Awake ()
+    {
+        if (!body) body = GetComponent<Rigidbody>();
+    }
+
+    void Start () {
+		
+}
+
+    Vector3 move;
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetAxis("Horizontal") != 0f || Input.GetAxis("Horizontal") != 0f)
+        {
+            move = new Vector3(Input.GetAxis("Horizontal") * speed, 0f, Input.GetAxis("Vertical") * speed);
+        }
+    }
+
+    
+
+    private void FixedUpdate()
+    {
+        body.velocity = move;
+    }
+}
