52dce1fbdde8222a754b4ddb9318b956d06ed12e
Tristan Jardon <tristan.jardon@gmail.com>
2017-11-13 11:52:27 +0100

    laser go forward

---
+++ laser.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class laser : MonoBehaviour {
+
+    [SerializeField]
+    Rigidbody body;
+
+    [SerializeField]
+    [Range(1f, 15f)]
+    float speed = 10f;
+
+
+    // Use this for initialization
+    void Awake() {
+        if (!body) body = GetComponent<Rigidbody>();
+        body.velocity = new Vector3(0f, 0f, speed);
+    }
+
+}
