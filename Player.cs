34e74b56babe0a6dd0a4c2ee3690630e5608ce08
Tristan Jardon <tristan.jardon@gmail.com>
2017-11-13 12:12:20 +0100

    Laser cooldown

--- Player.cs
+++ Player.cs
@@ -18,6 +18,10 @@ public class Player : MonoBehaviour {
     GameObject laserBullet;
     [SerializeField]
     Transform laserCanon;
+    [SerializeField]
+    [Range(1.0f, 2.0f)]
+    float coolDown = 1f;
+    float buffer = 0f;
 
     // Use this for initialization
     void Awake ()
@@ -26,13 +30,13 @@ public class Player : MonoBehaviour {
     }
 
     void Start () {
-		
-}
+        buffer = coolDown;
+		}
 
     Vector3 move;
     // Update is called once per frame
     void Update () {
-        if (Input.GetAxis("Horizontal") != 0f || Input.GetAxis("Horizontal") != 0f)
+        if (Input.GetAxis("Horizontal") != 0f || Input.GetAxis("Vertical") != 0f)
         {
             move = new Vector3(Input.GetAxis("Horizontal") * speed, 0f, Input.GetAxis("Vertical") * speed);
         }
@@ -40,14 +44,18 @@ public class Player : MonoBehaviour {
         {
             move = Vector3.zero;
         }
-
-        if (Input.GetButtonDown("Fire1")) {
-            Instantiate(laserBullet, laserCanon.position, laserCanon.rotation);
+        
+        if (buffer >= coolDown) {
+            if (Input.GetButtonDown("Fire1")) {
+                Instantiate(laserBullet, laserCanon.position, laserCanon.rotation);
+                buffer = 0f;
+            }
+        }
+        else {
+            buffer += Time.deltaTime;
         }
 
     }
-
-    
 
     private void FixedUpdate()
     {
